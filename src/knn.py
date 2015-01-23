c99039c9d7b75f809f6fc783da93385205dddefc
SZanlongo <SZanlongo@users.noreply.github.com>
2015-01-23 16:54:47 -0500

    Added: - determine type based on majrotiy knn values - errorRate and errorRateRange

--- src/knn.py
+++ src/knn.py
@@ -1,21 +1,28 @@
 # k-nearest neighbors
 # tailored to work on numbers represented by 256 values
 
+import time
 import math
 from operator import itemgetter
 
 ### Input Parameters ###
 trainingFile = "zip.train"
 testFile = "zip.test"
-k = 3
+
+k = 1
+
+dimS = 1
+dimE = 257
+
+trainS = 0
+trainE = 999
 
 ### Global Variables ###
 trainingPoints = []
 testPoints = []
 
 # Read files and split data by line
-# params:
-# filename
+# params: filename
 # returns: list with each line in file
 def readFile (filename):
     info = []
@@ -26,8 +33,7 @@ def readFile (filename):
     return info
 
 # Extract each number
-# params:
-# info - list of training point strings
+# params: info - list of training point strings
 # returns: list with training points
 def extractData(info):
     data = []
@@ -35,41 +41,36 @@ def extractData(info):
         pixels = []
         # Each number is represented by a set of 256 values
         for pixel in line:
-            pixels.append(pixel)
+            pixels.append(float(pixel))
         data.append(pixels)
     return data
 
 # Print input training points
-# params:
-# start, end - indexes for range to print
+# params: start, end - indexes for range to print
 def printPoints(start, end):
     for i in range(start, end):
         print trainingPoints[i]
 
 # Similarity given by ||x_test - x_train||
-# params:
-# a, b data points to compare
-# start, end - indexes for dimensions to compare
+# params: a, b data points to compare
 # returns: euclidean distance between two points
-def euclideanDistance(a, b, start, end):
+def euclideanDistance(a, b):
     d = 0
-    for i in range(start, end):
-        d += pow((a[i] - b[i]), 2)
-    d = math.sqrt(d)
-    
+#     for i in range(dimS, dimE):
+#         d += pow((a[i] - b[i]), 2)
+#     d = math.sqrt(d)
+    for i in range(dimS, dimE):
+        d += abs(a[i] - b[i])
+
     return d
 
 # Finds k nearest neighbors
-# params:
-# testValue - value to test for
-# k - number of neighbors to find
-# start, end - indexes for training point range
-# r1, r2 - indexes for dimensions to compare (for euclidean distance)
-# returns: k nearest neighbors
-def kNeighbors(testValue, k, start, end, r1, r2):
+# params: testValue - value to test for
+# returns: k nearest neighbors [value, distance, [dimensions]
+def kNeighbors(testValue):
     distances = []
-    for trainValue in trainingPoints[start:end]:
-        distances.append([euclideanDistance(testValue, trainValue, r1, r2), trainValue])
+    for trainValue in trainingPoints[trainS:trainE]:
+        distances.append([euclideanDistance(testValue, trainValue), trainValue[0], trainValue[dimS:dimE]])
     
     distances.sort(key = itemgetter(0))
     
@@ -79,5 +80,63 @@ def kNeighbors(testValue, k, start, end, r1, r2):
     
     return neighbors
 
+# Find closest match by majority
+# params: testValue - value to search for
+# returns: closest match 
+def byMajority(testValue):
+    count = dict()
+    neighbors = kNeighbors(testValue)
+    
+    # Find count of neighbors with each value
+    for neighbor in neighbors:
+        if neighbor[1] in count:
+            count[neighbor[1]] += 1
+        else:
+            count[neighbor[1]] = 1
+    return max(count.iteritems(), key = itemgetter(1))[0]
+
+# Find the percentage error of knn
+# returns: percentage error
+def checkError():
+    incorrect = 0.0
+    
+    for testPoint in testPoints:
+        if testPoint[0] != byMajority(testPoint):
+            incorrect += 1
+    
+    return (incorrect / len(testPoints)) * 100
+
+# Check error over a range of k's
+# params: start, end - range of k
+#        interval, every nth number is checked for k
+# returns: list of error percentages [k, percentage]
+def checkErrorRange(start, end, interval):
+    errorRate = []
+    ks = []
+    
+    for i in range(start, end + 1):
+        if i % interval != 0:
+            ks.append(i);
+    
+    for i in ks:
+        k = i
+        
+        intervalStart = time.clock()        
+        error = checkError()
+        intervalEnd = time.clock()
+        timeInterval = intervalEnd - intervalStart
+        
+        print k, error, timeInterval
+        errorRate.append([i, error, timeInterval])
+     
+    return errorRate
+
+# Plots errors given a set of k's and errors
+# params: errors - list of k's and errors [k, %]
+# def plotErrors(errors):
+    
+
 trainingPoints = extractData(readFile(trainingFile))
 testPoints = extractData(readFile(testFile))
+
+print checkErrorRange(1, 25, 2)
