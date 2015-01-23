85c89d0a98846150e58a9144a0640dd2fb413264
SZanlongo <SZanlongo@users.noreply.github.com>
2015-01-23 13:32:47 -0500

    Added k-nearest neighbors

--- src/knn.py
+++ src/knn.py
@@ -2,6 +2,7 @@
 # tailored to work on numbers represented by 256 values
 
 import math
+from operator import itemgetter
 
 ### Input Parameters ###
 trainingFile = "zip.train"
@@ -13,6 +14,9 @@ trainingPoints = []
 testPoints = []
 
 # Read files and split data by line
+# params:
+# filename
+# returns: list with each line in file
 def readFile (filename):
     info = []
     with open(filename, 'r') as openFile:
@@ -22,6 +26,9 @@ def readFile (filename):
     return info
 
 # Extract each number
+# params:
+# info - list of training point strings
+# returns: list with training points
 def extractData(info):
     data = []
     for line in info:
@@ -32,19 +39,45 @@ def extractData(info):
         data.append(pixels)
     return data
 
+# Print input training points
+# params:
+# start, end - indexes for range to print
 def printPoints(start, end):
     for i in range(start, end):
         print trainingPoints[i]
 
 # Similarity given by ||x_test - x_train||
+# params:
+# a, b data points to compare
+# start, end - indexes for dimensions to compare
+# returns: euclidean distance between two points
 def euclideanDistance(a, b, start, end):
     d = 0
     for i in range(start, end):
         d += pow((a[i] - b[i]), 2)
     d = math.sqrt(d)
+    
     return d
 
-
+# Finds k nearest neighbors
+# params:
+# testValue - value to test for
+# k - number of neighbors to find
+# start, end - indexes for training point range
+# r1, r2 - indexes for dimensions to compare (for euclidean distance)
+# returns: k nearest neighbors
+def kNeighbors(testValue, k, start, end, r1, r2):
+    distances = []
+    for trainValue in trainingPoints[start:end]:
+        distances.append([euclideanDistance(testValue, trainValue, r1, r2), trainValue])
+    
+    distances.sort(key = itemgetter(0))
+    
+    neighbors = []
+    for neighbor in range(0, k):
+        neighbors.append(distances[neighbor])
+    
+    return neighbors
 
 trainingPoints = extractData(readFile(trainingFile))
 testPoints = extractData(readFile(testFile))
